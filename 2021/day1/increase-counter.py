b8d92494b7feed6440c9dc2f969cb6f83ae0f7c7
Icedragon10532 <sebastianneiswanger@gmail.com>
2021-12-01 10:37:53 -0500

    day 1

---
+++ 2021/day1/increase-counter.py
@@ -0,0 +1,31 @@
+lines = []
+with open('input.txt') as f:
+    lines = f.readlines()
+
+count = 0
+lastnumber = 0
+for line in lines:
+    num = int(line)
+    if num > lastnumber:
+        count+=1
+    lastnumber = num
+count-=1
+
+print("Puzzle 1: ", count)
+
+count = 0
+lastnumber = 0
+place = 0
+for line in lines:
+    if place == 1998:
+        break
+    num = int(line)
+    num += int(lines[place+1])
+    num += int(lines[place+2])
+    if num > lastnumber:
+        count+=1
+    lastnumber = num
+    place+=1
+count-=1
+
+print("Puzzle 2: ", count)
